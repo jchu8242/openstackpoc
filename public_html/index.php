77d76b24ebc043868c83084478c6a9e8c345737d
root <root@localhost.localdomain>
2018-06-13 20:35:05 +1000

    login and IP

--- public_html/index.php
+++ public_html/index.php
@@ -3,6 +3,17 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
+    <form>
+        IP address:<br>
+        <input type="text" name="address"><br>
+        
+        username: <br>
+        <input type="text" name="username"><br>
+
+        password: <br>
+        <input type="text" name="password"><br><br>
+        
+        <input type="submit" value="Submit">
+    </form>
  </body>
 </html>
